abe15e14d4d2c2dbbcdcb15b21dad29bfea63d89
andriise <64638375+andriise@users.noreply.github.com>
2023-03-24 18:59:28 +0000

    Deploying to gh-pages from @ andriise/goit-react-hw-05-movies@deb4e9673a2c058c32dd14508545ee6f493c5fe4 🚀

--- static/js/115.a77a8acd.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[115],{115:function(e,i,t){t.r(i);var c=t(184);i.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("h1",{children:"MOVIES"})})}}}]);
-//# sourceMappingURL=115.a77a8acd.chunk.js.map
